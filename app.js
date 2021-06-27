6b07e02f12faeaec711dd3617fb142093f0af20f
andrei1dragos <76913543+andrei1dragos@users.noreply.github.com>
2021-06-27 20:39:15 +0100

    up

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const delInput = document.querySelector('.nameDel');
-const createBtn = document.querySelector('.createDel');
-const list = document.querySelector('.List');
-delInput.placeholder = 'London';
+const delInput1 = document.querySelector('.nameLdnDel');
+const delInput2 = document.querySelector('.nameOtherDel');
+const createBtn1 = document.querySelector('#createDel1');
+const createBtn2 = document.querySelector('#createDel2');
+const list1 = document.querySelector('#ldnList');
+const list2 = document.querySelector('#othersList');
 
-createBtn.addEventListener('click', (e)=>{
-    if(delInput.value != ""){
+createBtn1.addEventListener('click', (e)=>{
+    if(delInput1.value != ""){
         e.preventDefault();
 
         //create new row
         const newRow = document.createElement('li');
-        newRow.textContent = delInput.value;
+        newRow.textContent = delInput1.value;
         newRow.style.textTransform = 'uppercase';
-        list.appendChild(newRow);
+        list1.appendChild(newRow);
 
         //create observation input
         const observ = document.createElement('input');
-        observ.style.minWidth = '30rem'
+        observ.style.minWidth = '25rem'
         observ.style.right = '27rem';
+        observ.style.textTransform = 'lowercase'
         observ.placeholder = 'observation';
         newRow.appendChild(observ);
 
@@ -37,7 +40,57 @@ createBtn.addEventListener('click', (e)=>{
         newRow.appendChild(remBtn);
     }
 
-    delInput.value = '';
+    delInput1.value = '';
+
+    //create remove function
+    const remove = document.querySelectorAll('span')
+    for(let i=0; i< remove.length; i++){
+        remove[i].addEventListener('click', ()=>{
+            remove[i].parentElement.style.opacity = '0';
+            setTimeout(()=>{
+                remove[i].parentElement.style.display = 'none';
+                remove[i].parentElement.remove();
+            },500);
+        })
+    }
+})
+
+createBtn2.addEventListener('click', (e)=>{
+    if(delInput2.value != ""){
+        e.preventDefault();
+
+        //create new row
+        const newRow = document.createElement('li');
+        newRow.textContent = delInput2.value;
+        newRow.style.textTransform = 'uppercase';
+        list2.appendChild(newRow);
+
+        //create observation input
+        const observ = document.createElement('input');
+        observ.style.minWidth = '25rem'
+        observ.style.right = '27rem';
+        observ.style.textTransform = 'lowercase'
+        observ.placeholder = 'observation';
+        newRow.appendChild(observ);
+
+        //create priority input
+        const priority = document.createElement('input');
+        priority.style.right = '15rem';
+        priority.placeholder = 'priority';
+        newRow.appendChild(priority);
+
+        //create driver input
+        const driver = document.createElement('input');
+        driver.placeholder = 'driver';
+        newRow.appendChild(driver);
+
+        //create remove button
+        const remBtn = document.createElement('span');
+        remBtn.innerHTML = 'x';
+        newRow.appendChild(remBtn);
+    }
+
+    delInput2.value = '';
 
     //create remove function
     const remove = document.querySelectorAll('span')
